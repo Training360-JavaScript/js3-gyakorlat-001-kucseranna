a7c01dc8a2f61afb8d0e501a9781ee73ee6fbdec
kucseranna <kucseranna@gmail.com>
2021-11-09 17:59:12 +0100

    done

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,7 @@
-
-
-
-
-
-
-// export default removeDuplicatesFromArray
+const removeDuplicatesFromArray = (arr) => {
+    let arrWoDuplicates = arr.filter((element, index) => {
+        return arr.indexOf(element) === index;
+    })
+    return arrWoDuplicates;
+};
+export default removeDuplicatesFromArray
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,17 @@
+const HU = {
+    date(anyDate) {
+        return new Intl.DateTimeFormat('hu-HU').format(anyDate);
+    },
+    curreny(amount) {
+        return new Intl.NumberFormat('hu-HU', {
+            style: 'currency',
+            currency: 'HUF'
+        }).format(amount);
+    },
 
-
-
-
-
-
-// export default HU
+    list(arr) {
+        return arr.map((item, i) => i === arr.length-1 ? ` és ${item}` : `, ${item}`)
+            .join('').slice(2);
+    }
+};
+export default HU
